bc35ea642b0fc5747a931463d443a96bdf8cbb89
Hitesh Patel <hitesh@pixelhexdigital.com>
2024-01-04 02:05:02 +0530

    react setup

---
+++ src/pages/HomePage/Data.js
@@ -0,0 +1,67 @@
+export const homeObjOne = {
+  primary: true,
+  lightBg: false,
+  lightTopLine: true,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: "BNM India: Business Networking for Growth",
+  headline: "Connect with GST-Verified Professionals",
+  description:
+    "Elevate your business by connecting with like-minded individuals, sharing ideas, and exchanging services on our exclusive platform.",
+  buttonLabel: "Get Started",
+  imgStart: "",
+  img: require("../../images/svg-1.svg"),
+  alt: "Credit Card",
+  start: "",
+};
+
+export const homeObjTwo = {
+  primary: true,
+  lightBg: false,
+  lightTopLine: true,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: "Instant Setup",
+  headline: "Quick Onboarding for Immediate Networking",
+  description:
+    "Join BNM India, and our specialists will guide you through a rapid onboarding process, getting you set up to network within minutes.",
+  buttonLabel: "Learn More",
+  imgStart: "",
+  img: require("../../images/svg-2.svg"),
+  alt: "Vault",
+  start: "",
+};
+
+export const homeObjThree = {
+  primary: false,
+  lightBg: true,
+  lightTopLine: false,
+  lightText: false,
+  lightTextDesc: false,
+  topLine: "Success Stories",
+  headline: "Discover Success: Transforming Revenue with BNM India",
+  description:
+    "Explore real success stories like Sarah Jeni, who experienced a 3X revenue increase in less than 3 months. View case studies now.",
+  buttonLabel: "View Case Studies",
+  imgStart: "start",
+  img: require("../../images/profile.jpg"),
+  alt: "Vault",
+  start: "true",
+};
+
+export const homeObjFour = {
+  primary: true,
+  lightBg: false,
+  lightTopLine: true,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: "Secure Networking",
+  headline: "Your Data, Our Priority",
+  description:
+    "Rest easy with BNM India. Your data is stored securely, guarded by our expert team. Sign up now to experience safe and productive networking.",
+  buttonLabel: "Sign Up Now",
+  imgStart: "start",
+  img: require("../../images/svg-3.svg"),
+  alt: "Vault",
+  start: "true",
+};
